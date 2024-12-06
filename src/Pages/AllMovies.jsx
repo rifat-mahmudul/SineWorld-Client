373e530fdf915c23be2c9016120601f62b97e460
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-12-06 12:33:46 +0600

    search done

--- src/Pages/AllMovies.jsx
+++ src/Pages/AllMovies.jsx
@@ -1,10 +1,28 @@
 import { Helmet } from "react-helmet-async";
 import Card from "../components/Shared/Card";
 import useMovies from "../Hooks/useMovies"
+import { useEffect, useState } from "react";
 
 const AllMovies = () => {
 
     const [movies] = useMovies();
+    const [movieData, setMovieData] = useState(movies)
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/movies?search=${search}`)
+        .then(res => res.json())
+        .then(data => {
+            setMovieData(data);
+        })
+    }, [search])
+
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const searchMovie = e.target.value;
+        setSearch(searchMovie)
+    }
 
     return (
         <section>
@@ -14,17 +32,24 @@ const AllMovies = () => {
 
             <div className="mt-24 mb-4 text-center">
                 <input 
+                onChange={handleSearch}
                 className="border-2 border-orange-600 w-[90%] sm:w-[350px] p-3 rounded-lg text-xl font-semibold" 
                 type="text" 
                 placeholder="Search Movie" 
                 />
             </div>
 
+            <div>
+                {
+                    movieData.length === 0 && <p className="text-center text-4xl text-red-500 font-bold">NO DATA FOUND</p>
+                }
+            </div>
+
             <div 
             className="pt-4 max-w-[90%] xl:max-w-[1200px] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center justify-between gap-8 mb-16"
             >
                 {
-                    movies.map(movie => <Card key={movie._id} movie={movie}></Card>)
+                    movieData.map(movie => <Card key={movie._id} movie={movie}></Card>)
                 }
             </div>
         </section>
