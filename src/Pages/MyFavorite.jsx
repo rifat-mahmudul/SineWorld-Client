51c965cf29a8077a574d9cb84b465758a0496118
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-12-07 18:49:09 +0600

    loading spinner on my favorite page

--- src/Pages/MyFavorite.jsx
+++ src/Pages/MyFavorite.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react"
 import { Helmet } from "react-helmet-async"
 import useAuth from "../Hooks/useAuth";
 import FavoriteCard from "../components/Shared/FavoriteCard";
+import FadeLoader from "react-spinners/FadeLoader";
 
 const MyFavorite = () => {
 
     const [movies, setMovies] = useState([]);
-    const {user} = useAuth();
+    const {user, loading} = useAuth();
 
     useEffect(() => {
         fetch(`https://assignment-10-server-delta-sand.vercel.app/favorite/${user?.email}`)
@@ -24,6 +25,11 @@ const MyFavorite = () => {
 
             <div>
                 {
+                    loading ?
+                    <div className="flex justify-center items-center h-[80vh] text-yellow-600">
+                        <FadeLoader color="#ca8a04" />
+                    </div>
+                    :
                     movies.length === 0 && <p className="text-center text-4xl text-red-500 font-bold">NO DATA FOUND</p>
                 }
             </div>
@@ -32,6 +38,11 @@ const MyFavorite = () => {
             className="pt-4 max-w-[90%] xl:max-w-[1200px] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-center justify-between gap-8 mb-16"
             >
                 {
+                    loading ?
+                    <div className="flex justify-center items-center h-[80vh] text-yellow-600">
+                        <FadeLoader color="#ca8a04" />
+                    </div>
+                    :
                     movies.map(movie => <FavoriteCard key={movie._id} movie={movie} movies={movies} setMovies={setMovies}></FavoriteCard>)
                 }
             </div>
