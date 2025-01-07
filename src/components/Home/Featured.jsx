d788438a66873e5eece1a82b1952298336fae2a4
rifat-mahmudul <mahmudulhasanrifat220@gmail.com>
2025-01-07 10:18:26 +0600

    show all movie info in home page by dynamic custom hooks

--- src/components/Home/Featured.jsx
+++ src/components/Home/Featured.jsx
@@ -1,12 +1,6 @@
-import Title from "../Shared/Title"
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Keyboard, FreeMode } from 'swiper/modules';
-import { Link } from "react-router";
+import RecentAdded from "../Shared/Featured/RecentAdded";
 
 const Featured = () => {
 
@@ -25,43 +19,7 @@ const Featured = () => {
     return (
         <section className="pb-24">
             
-
-            <div className="mt-10 max-w-[90%] xl:max-w-[1200px] mx-auto">
-
-                <div className="mb-5">
-                    <Title title="Enjoy some amazing movies" heading="Recently Added"></Title>
-                </div>
-
-                <Swiper
-                slidesPerView={6}
-                spaceBetween={70}
-                freeMode={true}
-                navigation={true}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[FreeMode, Keyboard, Navigation]}
-                className="mySwiper  h-[280px]"
-                >
-
-                    {
-                        recentAdded.map(item => (
-                            <SwiperSlide key={item.key} className="h-[250px] w-[180px] ">
-                                <Link to={`/Details/${item?._id}`}>
-                                    <div className="h-[250px] w-[200px]  rounded-lg ">
-                                        <div>
-                                            <img className="rounded-lg w-full h-[250px]" src={item?.MoviePoster} alt="img" />
-                                        </div>
-
-                                        <h1 className='mt-2 text-gray-300'>{item?.MovieTitle}</h1>
-                                    </div>
-                                </Link>
-                            </SwiperSlide>
-                        ))
-                    }
-
-                </Swiper>
-            </div>
+            <RecentAdded recentAdded={recentAdded}></RecentAdded>
         
         </section>
     )
---
+++ src/components/Shared/Featured/RecentAdded.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable react/prop-types */
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Keyboard, FreeMode } from 'swiper/modules';
+import { Link } from "react-router";
+import Title from '../Title';
+
+const RecentAdded = ({ recentAdded }) => {
+    return (
+        <div className="mt-10 max-w-[90%] xl:max-w-[1200px] mx-auto">
+
+            <div className="mb-5">
+                <Title title="Enjoy some amazing movies" heading="Recently Added"></Title>
+            </div>
+
+            <Swiper
+                slidesPerView={6} 
+                freeMode={true}
+                navigation={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[FreeMode, Keyboard, Navigation]}
+                className="mySwiper h-[280px]"
+                breakpoints={{
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+                    640: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 30,
+                    },
+                    1280: {
+                        slidesPerView: 6,
+                        spaceBetween: 10,
+                    },
+                }}
+            >
+                {
+                    recentAdded.map(item => (
+                        <SwiperSlide key={item.key} className="h-[250px] w-[180px]">
+                            <Link to={`/Details/${item?._id}`}>
+                                <div className="h-[260px] w-[180px] rounded-lg hover:scale-105 transition">
+                                    <div>
+                                        <img className="rounded-lg w-full h-[250px]" src={item?.MoviePoster} alt="img" />
+                                    </div>
+                                    <h1 className="mt-2 text-gray-300">{item?.MovieTitle}</h1>
+                                </div>
+                            </Link>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </div>
+    );
+};
+
+export default RecentAdded;
